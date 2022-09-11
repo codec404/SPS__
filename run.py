fdede2a12af09d9ac595045b429218110e98dbb4
codec404 <saptarshimemari072@gmail.com>
2022-09-11 17:27:54 +0530

    Running app

--- run.py
+++ run.py
@@ -1,2 +1,18 @@
-from game import game
-game()
+from game import Score_display, game ,finalScore
+from scores import scoreboard
+options=["STONE","PAPER","SCISSOR"]
+round = 1
+print("Enter your name:")
+Name=input()
+computer=0
+player=0
+
+Score_display(computer,player,Name)
+print("Hello",Name)
+game(options,Name,round,computer,player)
+
+Score = finalScore()
+print()
+print(">>>>>SCOREBOARD<<<<<")
+print()
+scoreboard(Name,Score)
